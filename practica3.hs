9f491371d8cb4d87ddf7e36072983da80e0bde75
José María Hernández <jmhdpz@hotmail.com>
2024-10-04 20:49:03 -0600

    ya acabe

--- practica3.hs
+++ practica3.hs
@@ -25,16 +25,25 @@ conjunto (Node x lista) = if estaContenido lista x
                           else Node x (conjunto lista)
 
 eliminarIndice :: List a -> Int -> List a
-eliminarIndice Void n = error "Lista vacia"
-eliminarIndice (Node x lista) 0 = lista
-eliminarIndice (Node x lista) n = if (n > longitud (Node x lista) -1 || 0 > n)
-                                    then error "Indice no valido"
-                                    else Node x (eliminarIndice lista (n-1))
+eliminarIndice Void n = error "Lista vacía"
+eliminarIndice (Node x lista) n = if n < 0 || n >= longitud (Node x lista)
+                                    then error "Indice invalido"
+                                    else if n == 0
+                                        then lista 
+                                        else Node x (eliminarIndice lista (n-1))
 
 insertarIndice :: List a -> Int -> a -> List a
-insertarIndice = undefined
+insertarIndice Void n a = if n /= 0
+                            then error "Indice invalido (unico indice valido en lista vacia es 0)"
+                            else (Node a Void)
+insertarIndice (Node x lista) n a = if n < 0 || n > longitud (Node x lista)
+                                        then error "Indice invalido"
+                                        else if n == 0
+                                        then Node a (Node x lista)    
+                                        else Node x (insertarIndice lista (n-1) a)           
 
 recorrerLista :: List a -> Int -> List a
-recorrerLista = undefined
+recorrerLista lista 0 = lista
+recorrerLista (Node x lista) n = recorrerLista (insertarIndice lista (longitud lista) x) (n-1)
 
 
